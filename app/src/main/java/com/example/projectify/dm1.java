b25768c98f4361146678024a60fa5a518d48bce9
Sahan7 <sahan.pasindupeiris7@gmail.com>
2022-12-28 00:03:35 +0530

    name1chat

---
+++ app/src/main/java/com/example/projectify/dm1.java
@@ -0,0 +1,14 @@
+package com.example.projectify;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class dm1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dm1);
+    }
+}
